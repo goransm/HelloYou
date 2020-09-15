f659fff0f3ebd6adc8a87f741d583d787769cefc
Gøran Smistad <goran@smistad.org>
2020-09-15 11:52:08 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloYou
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello You!");
+            Console.WriteLine("My name is Gøran, what's yours?");
+            string name = Console.ReadLine();
+            int nameLength = name.Length;
+            char firstLetter = name[0];
+            Console.WriteLine($"Hello {name}, your name is {nameLength} characters long and starts with an {firstLetter}", name, nameLength, firstLetter);
+        }
+    }
+}
